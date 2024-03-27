d6a54d39fff8a9e5693ad2dfb291af443abed06e
farazmajid56 <farazmajid56@gmail.com>
2024-03-27 15:40:42 +0500

    Added Fixes for Card Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Card from './Components/Card/Card.js'
 import Header from './Components/Header/Header.js'
 
 function App() {
-  
+  const data = [
+    {path:"https://www.flashfootwear.com.pk/wp-content/uploads/2021/01/Blue-Ecosense-Lightning-Shoes-for-Kids-1.jpg",name:"Running Shoes",price:5000,isSold:true},
+    {path:"https://5.imimg.com/data5/SELLER/Default/2022/11/KE/VX/MV/116453489/white-casual-shoes-for-men-500x500.jpg",name:"Casual Shoes",price:2500,isSold:false},
+    {path:"https://m.media-amazon.com/images/I/71qgiMBkSXL._AC_SL1200_.jpg",name:"Formal Shoes",price:1750,isSold:false}
+  ]
   return (
     <>
       <Header />
-      <Card path="../src/Assets/RunningShoes.jpg" name="Running Shoes" price = {5000}/>
+      {data.map( x => <Card path={x.path} name={x.name} price={x.price} isSold={x.isSold} />)}
     </>
   );
 }
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,10 +2,11 @@ import './Card.css';
 
 function Card(props){
     return (
-        <div>
-            <image src={props.path}/>
+        <div id="card">
+            <img src={props.path} alt={props.name} />
             <h3>{props.name}</h3>
             <p>{props.price}</p>
+            {props.isSold ? <p className='sold'>Sold</p> : <p className="inStock">In Stock</p>}
         </div>
     )
 }
