f0367263e9119d50b5669ce6e2ae33befb50840f
farazmajid56 <farazmajid56@gmail.com>
2024-03-30 10:52:52 +0500

    Added Improvements to Card Component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,10 +3,10 @@ import './Card.css';
 function Card(props){
     return (
         <div id="card">
+            {props.isSold ? <p className='sold'>Sold</p> : <p className="inStock">In Stock</p>}
             <img src={props.path} alt={props.name} />
             <h3>{props.name}</h3>
             <p>{props.price}</p>
-            {props.isSold ? <p className='sold'>Sold</p> : <p className="inStock">In Stock</p>}
         </div>
     )
 }
